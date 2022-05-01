8e42ecb168ed582fcac327ee65cfe1bdb46b0810
Abhinav Yadav <mr.kumarabhinavyadav@gmail.com>
2022-05-02 01:12:46 +0530

    solve

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 const app = express();
-const cors = require("cors");
-const path = require("path");
-const dotenv = require("dotenv");
+
+const dotenv = require('dotenv')
+
 
 dotenv.config();
-const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production") {
-    app.use(cors({ origin: "*" }));
+app.use(express.static(path.join(__dirname, 'build')));
 
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-        req.sendFile(path.resolve(__dirname, "build", "index.html"));
-    });
-}
-
-app.listen(port, (err) => {
-    if (err) return console.log(err);
-    console.log("Server is running on port : ", port);
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+app.listen(process.env.PORT || 3000);
