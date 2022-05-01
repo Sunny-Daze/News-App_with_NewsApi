da779011e6c0d9b73084fe7a33a5db8ee8fc0a3c
Abhinav Yadav <mr.kumarabhinavyadav@gmail.com>
2022-05-02 01:04:30 +0530

    local

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const path = require("path");
+const dotenv = require("dotenv")
 
 
 
 
+dotenv.config();
 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV === "production") {
     app.use(cors({ origin: "*" }));
