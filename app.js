9cff059c7b04c93136da001d9cb7297555516615
Sunny-Daze7 <sunnyadav0727@gmail.com>
2022-05-02 01:03:52 +0530

    final

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const path = require("path");
+const dotenv = require("dotenv");
 
-
-
-
-
-
+dotenv.config();
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
-    app.use(cors({ origin: "*" }));
+  app.use(cors({ origin: "*" }));
 
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-        req.sendFile(path.resolve(__dirname, "build", "index.html"));
-    });
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    req.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
 }
 
 app.listen(port, (err) => {
-    if (err) return console.log(err);
-    console.log("Server is running on port : ", port);
+  if (err) return console.log(err);
+  console.log("Server is running on port : ", port);
 });
