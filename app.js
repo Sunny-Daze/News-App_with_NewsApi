40aee44185406ad50cf22076f9164c2557207a8a
Abhinav Yadav <mr.kumarabhinavyadav@gmail.com>
2022-05-02 01:31:11 +0530

    commit

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors');
+
+
+
+
 
 const dotenv = require('dotenv')
 
 
 dotenv.config();
+
+app.use(cors({ origin: "*" }));
 
 app.use(express.static(path.join(__dirname, 'build')));
 
